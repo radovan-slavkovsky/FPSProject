9f803b305f87255b33d4bf6bcd926ceb6f24ae8d
DaDuum <radovan.slavkovsky@gmail.com>
2021-02-03 21:47:29 +0100

    Crosshair added

--- Intermediate/Build/Win64/UE4Editor/Inc/FPSProject/FPSProject.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/FPSProject/FPSProject.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeFPSProject_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x2A077D74,
-				0xA99A01CC,
+				0x7273519F,
+				0x511A47F6,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
